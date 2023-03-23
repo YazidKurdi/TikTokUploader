d76c4ddfa835d5ced3d8f235cec6406bb5bd5f62
Yazid <yazeedkurdi@hotmail.com>
2023-03-24 00:22:46 +0300

    youtube_api

--- youtube_API.py
+++ youtube_API.py
@@ -1,59 +1,80 @@
+import configparser
+import json
 import os
+import config
 import pickle
 import re
 from googleapiclient.discovery import build
 
-def duration_string_to_seconds(duration_string):
-    match = re.match('PT(\d+H)?(\d+M)?(\d+S)?', duration_string)
-    hours = int(match.group(1)[:-1]) if match.group(1) else 0
-    minutes = int(match.group(2)[:-1]) if match.group(2) else 0
-    seconds = int(match.group(3)[:-1]) if match.group(3) else 0
-    return hours * 3600 + minutes * 60 + seconds
+class YouTubeVideoSaver:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.youtube = build('youtube', 'v3', developerKey=config.youtube_api)
+        self.file_path = "videos_to_download.pickle"
 
-# Set up the API client
-api_key = 'AIzaSyDNYvOgovBehCXZa9aFnc11fT7rarX1HTg'
-youtube = build('youtube', 'v3', developerKey=api_key)
+        if os.path.isfile("videos_to_download.pickle"):
+            with open("videos_to_download.pickle", "rb") as f:
+                self.videos = pickle.load(f)
+                self.videos = json.loads(self.videos)
+        else:
+            self.videos = {}
 
+    def duration_string_to_seconds(self, duration_string):
+        match = re.match('PT(\d+H)?(\d+M)?(\d+S)?', duration_string)
+        hours = int(match.group(1)[:-1]) if match.group(1) else 0
+        minutes = int(match.group(2)[:-1]) if match.group(2) else 0
+        seconds = int(match.group(3)[:-1]) if match.group(3) else 0
+        return hours * 3600 + minutes * 60 + seconds
 
-# Load the saved videos dictionary from disk, if it exists
-if os.path.isfile("videos_to_download.pickle"):
-    with open("videos_to_download.pickle", "rb") as f:
-        videos = pickle.load(f)
-else:
-    videos = {}
+    def return_channel_metadata(self,channel_id,max_results=50):
+        # Call the YouTube API to get the most popular short videos for the given channel ID
+        request = self.youtube.search().list(
+            part="id,snippet",
+            channelId=channel_id,
+            maxResults=max_results,
+            type="video",
+            order="viewCount"
+        )
+        response = request.execute()
 
-# Set up the search parameters
-channel_id = 'UCQwkEQ6EQc4jM2QnrvubBHg'
-max_results = 50
-order_by = "viewCount"
+        for item in response['items']:
+            video_id = item['id']['videoId']
+            video_duration = self.youtube.videos().list(id=video_id, part='contentDetails').execute()['items'][0]['contentDetails']['duration']
+            duration_in_seconds = self.duration_string_to_seconds(video_duration)
+            if duration_in_seconds <= 600:
+                video_title = item['snippet']['title']
+                video_url = f"https://www.youtube.com/watch?v={video_id}"
+                channel_title = item['snippet']['channelTitle']
+                is_short = duration_in_seconds <= 60
+                video_data = {'title': video_title, 'url': video_url, 'is_short': is_short}
+                if channel_title not in self.videos:
+                    self.videos[channel_title] = [video_data]
+                else:
+                    self.videos[channel_title].append(video_data)
 
-# Call the YouTube API to get the most popular short videos for the given channel ID
-request = youtube.search().list(
-    part="id,snippet",
-    channelId=channel_id,
-    maxResults=max_results,
-    type="video",
-    order=order_by
-)
-response = request.execute()
+    def save_videos_to_file(self):
+        # convert the dictionary to JSON
+        json_data = json.dumps(self.videos, indent=4)
 
-# Update the videos dictionary with any new videos we found
-for item in response['items']:
-    video_id = item['id']['videoId']
-    video_duration = youtube.videos().list(id=video_id, part='contentDetails').execute()['items'][0]['contentDetails']['duration']
-    duration_in_seconds = duration_string_to_seconds(video_duration)
-    if duration_in_seconds <= 600:
-        video_title = item['snippet']['title']
-        video_url = f"https://www.youtube.com/watch?v={video_id}"
-        if video_url not in videos:
-            videos[video_url] = video_title
+        # Save the updated videos dictionary to disk
+        with open(self.file_path, "wb") as f:
+            pickle.dump(json_data, f)
 
+    def process_all_channels(self):
+        config = configparser.ConfigParser()
+        config.read('channels.ini')
 
-# Save the updated videos dictionary to disk
-with open("videos_to_download.pickle", "wb") as f:
-    pickle.dump(videos, f)
+        for section in config.sections():
+            for channel_id in config[section].values():
+                print(channel_id)
+                self.return_channel_metadata(channel_id)
+        self.save_videos_to_file()
 
-# Print out the list of video titles and URLs
-for video_url, video_title in videos.items():
-    print(f"{video_title}: {video_url}")
+    def print_video_titles_and_urls(self):
+        # Print out the list of video titles and URLs
+        for channel_title, videos in self.videos.items():
+            for video in videos:
+                print(f"{video['title']}: {video['url']}")
 
+l = YouTubeVideoSaver(config.youtube_api)
+l.process_all_channels()
